899af86d74b70f3cf5e42285c00fa7e36d22f66e
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-12 00:28:22 +0200

    feat: casi di prova dell'istanza della classe

--- src/main/java/org/lessons/java/shop/Main.java
+++ src/main/java/org/lessons/java/shop/Main.java
@@ -1,12 +1,28 @@
 package org.lessons.java.shop;
 
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 
+	
+    //creazione di un nuovo oggetto di classe Prodotto
+	Prodotto prodotto1 = new Prodotto("Produttone", "bel prodotto", 30.55, 20);
+	
+	//stampa del prezzo del prodotto
+	System.out.println(prodotto1.getPrice());
+	
+	//stampa del prezzo del prodotto comprensivo di IVA
+	System.out.println(prodotto1.getFullPrice());
+	
+	//stampa del nome completo del prodotto in formato codice-nome
+	System.out.println(prodotto1.getFullName());
+	
+	//stampa della descrizione del prodotto
+	System.out.println(prodotto1.description);
+	
+	
 	}
-
-	Prodotto prodotto1 = new Prodotto();
-	Prodotto prodotto2 = new Prodotto();
 }
